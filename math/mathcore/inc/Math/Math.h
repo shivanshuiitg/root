45d34eca288ab949d228f48733db242125a0f7f5
Xavier Valls <xaviervallspla@gmail.com>
2017-02-27 15:52:23 +0100

    Move KahanSum to Math/Util.h

--- math/mathcore/inc/Math/Math.h
+++ math/mathcore/inc/Math/Math.h
@@ -113,39 +113,6 @@ namespace ROOT {
       #endif
       }
 
-      template<class T>
-      class KahanSum {
-      public:
-         void Add(const T &x)
-         {
-            auto y = x - correction;
-            auto t = sum + y;
-            correction = (t - sum) - y;
-            sum = t;
-         }
-
-         void Add(const std::vector<T> &elements)
-         {
-            for (auto e : elements)
-               this->Add(e);
-         }
-
-         static double Accumulate(const std::vector<T> &elements)
-         {
-            KahanSum init;
-            init.Add(elements);
-            return init.sum;
-         }
-
-         double Result()
-         {
-            return sum;
-         }
-
-      private:
-         double sum = 0.;
-         double correction = 0.;
-      };
    } // end namespace Math
 
 } // end namespace ROOT
